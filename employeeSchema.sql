7eccdd5681200018373ad0fe0bb15a6dc0c6d806
Chris Lee <cil5345@gmail.com>
2020-03-09 23:04:14 -0400

    created propmts with inquirer

---
+++ employeeSchema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+
+-- table for departments
+CREATE TABLE department(
+id INT(10) AUTO_INCREMENT NOT NULL, 
+name VARCHAR(30),
+PRIMARY KEY(id)
+);
+
+-- table for role
+CREATE TABLE role(
+id INT(10) AUTO_INCREMENT NOT NULL,
+title VARCHAR(30),
+salary DECIMAL(10000000, 2),
+department_id INT(30),
+PRIMARY KEY(id)
+);
+
+-- table for employee
+CREATE TABLE employee(
+id INT(10) AUTO_INCREMENT NOT NULL,
+firstName VARCHAR(30),
+lastName VARCHAR(30),
+role_id INT(30),
+manager_id INT(30),
+PRIMARY KEY(id)
+);
---
+++ server.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "118136aaaA",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+    askQuestion();
+});
+
+let listQuestions = false
+function askQuestion() {
+    if (listQuestions === false) {
+        inquirer
+            .prompt([
+                {
+                    message: "What would you like to do",
+                    name: "initialprompt",
+                    type: "list",
+                    choices: ["View All Employees", "View All Employees by Department", "View all Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "view All Roles"]
+                }
+            ]);
+
+
+    }
+
+
+}
